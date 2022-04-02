39a957e979f49368db81ecec7036a7f51b40f5d8
sunyixin <yixinsun@andrew.cmu.edu>
2022-04-02 12:48:45 -0700

    SpringBoot init

---
+++ src/test/java/com/yixin/aggrid/AgGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yixin.aggrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgGridApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
